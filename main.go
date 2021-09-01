fbd81bbd48089046b692a0a490d2eda855350094
akez <1791189210@qq.com>
2021-09-01 16:31:12 +0800

    init

--- main.go
+++ main.go
@@ -1,49 +1,20 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/gin-gonic/gin"
-	"github.com/tencentyun/scf-go-lib/cloudfunction"
-	"github.com/tencentyun/scf-go-lib/events"
+	"log"
+	"net/http"
 )
 
-type Body struct {
-	Code int
-	Msg  string
-}
-
-func hello() (events.APIGatewayResponse, error) {
-	body := Body{
-		Code: 200,
-		Msg:  "success",
-	}
-	marshal, err := json.Marshal(body)
-	if err != nil {
-		return events.APIGatewayResponse{}, err
-	}
-
-	if err != nil {
-		return events.APIGatewayResponse{}, err
-	}
+func main() {
 	server := gin.Default()
 	server.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "success",
 		})
-		return
 	})
-	err = server.Run(":9000")
+	err := server.Run("0.0.0.0:9000")
 	if err != nil {
-		return events.APIGatewayResponse{}, err
+		log.Fatalln("server run error")
 	}
-	return events.APIGatewayResponse{
-		IsBase64Encoded: false,
-		StatusCode:      200,
-		Headers:         nil,
-		Body:            string(marshal),
-	}, nil
-}
-
-func main() {
-	cloudfunction.Start(hello)
 }
