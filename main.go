7a805cc7580f73d71ff46b8cd46d2a52c9218cf0
akez <1791189210@qq.com>
2021-09-01 14:38:32 +0800

    init

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
 	"github.com/tencentyun/scf-go-lib/events"
 )
 
-func hello(ctx context.Context, req events.APIGatewayRequest) events.APIGatewayResponse {
+func hello(ctx context.Context, req events.APIGatewayRequest) (events.APIGatewayResponse, error) {
 	return events.APIGatewayResponse{
 		IsBase64Encoded: false,
 		StatusCode:      200,
 		Headers:         nil,
 		Body:            "{msg: success}",
-	}
+	}, nil
 }
 
 func main() {
