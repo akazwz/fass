f95ab95d51a78b969b3139880e30cd03e30a3992
akez <1791189210@qq.com>
2021-09-01 14:48:50 +0800

    init

--- main.go
+++ main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/tencentyun/scf-go-lib/cloudfunction"
 	"github.com/tencentyun/scf-go-lib/events"
 )
 
+type Body struct {
+	Code int
+	Msg  string
+}
+
 func hello(ctx context.Context, req events.APIGatewayRequest) (events.APIGatewayResponse, error) {
+	body := Body{
+		Code: 200,
+		Msg:  "success",
+	}
+	marshal, err := json.Marshal(body)
+	if err != nil {
+		return events.APIGatewayResponse{}, err
+	}
+
+	if err != nil {
+		return events.APIGatewayResponse{}, err
+	}
 	return events.APIGatewayResponse{
 		IsBase64Encoded: false,
 		StatusCode:      200,
 		Headers:         nil,
-		Body:            "{msg: success}",
+		Body:            string(marshal),
 	}, nil
 }
 
